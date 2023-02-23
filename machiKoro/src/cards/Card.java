c0c6666741fa66d36d70e3a21d0058d7a86ee57e
Jake Swanson <jakeswan213@gmail.com>
2023-02-22 22:49:38 -0600

    Added cards and info

--- machiKoro/src/cards/Card.java
+++ machiKoro/src/cards/Card.java
@@ -1,8 +1,18 @@
 package cards;
 
-public abstract class Card {
+public class Card {
 
 	private int cost;
 	private String name;
+	private String description;
+	private int[] activationRolls;
+	
+	public Card(int c, String n, String d, int[] nums) {
+		this.cost = c;
+		this.name = n;
+		this.description = d;
+		this.activationRolls = nums;
+	}
+	
 	
 }
